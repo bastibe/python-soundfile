5c66da5fcddc2c310a2fef964787360d540d20eb
David Blewett <davidb@sixfeetup.com>
2013-11-28 12:04:35 -0500

    Actually call `self._init_vio`...

--- pysoundfile.py
+++ pysoundfile.py
@@ -270,56 +270,12 @@ class SoundFile(object):
 
         if virtual_io:
             fObj = name
-
             for attr in ('seek', 'read', 'write', 'tell'):
                 if not hasattr(fObj, attr):
                     msg = 'File-like object must have: "%s"' % attr
                     raise RuntimeError(msg)
-            elif hasattr(fObj, '_length'):
-                size = fObj._length
-            elif not hasattr(fObj, '__len__'):
-                old_file_position = fObj.tell()
-                fObj.seek(0, os.SEEK_END)
-                size = fObj.tell()
-                fObj.seek(old_file_position, os.SEEK_SET)
-            else:
-                size = len(fObj)
-
-            # Define callbacks here, so they can reference fObj / size
-            @ffi.callback("sf_vio_get_filelen")
-            def vio_get_filelen(user_data):
-                return size
-
-            @ffi.callback("sf_vio_seek")
-            def vio_seek(offset, whence, user_data):
-                fObj.seek(offset, whence)
-                curr = fObj.tell()
-                return curr
-
-            @ffi.callback("sf_vio_read")
-            def vio_read(ptr, count, user_data):
-                buf = ffi.buffer(ptr, count)
-                data_read = fObj.readinto(buf)
-                return data_read
-
-            @ffi.callback("sf_vio_write")
-            def vio_write(ptr, count, user_data):
-                buf = ffi.buffer(ptr)
-                data = buf[:]
-                length = fObj.write(data)
-                return length
-
-            @ffi.callback("sf_vio_tell")
-            def vio_tell(user_data):
-                return fObj.tell()
-
-            vio = ffi.new("SF_VIRTUAL_IO*")
-            vio.get_filelen = vio_get_filelen
-            vio.seek = vio_seek
-            vio.read = vio_read
-            vio.write = vio_write
-            vio.tell = vio_tell
-            self._file = _snd.sf_open_virtual(vio, self._file_mode, info,
+            self._vio = self._init_vio()
+            self._file = _snd.sf_open_virtual(self._vio, self._file_mode, info,
                                               ffi.NULL)
         else:
             filename = ffi.new('char[]', name.encode())
