37756c879f17b6a6624975b58986c546420462a5
Matthias Geier <Matthias.Geier@gmail.com>
2014-07-15 13:14:47 +0200

    Create test WAV files using the struct module

--- tests/generate_soundfiles.py
+++ tests/generate_soundfiles.py
@@ -1,16 +1,73 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
-import platform
-import numpy as np
-from scipy.io import wavfile
+import struct
 
-# SciPy >= 0.13 is needed for 32-bit floating point support
 
-wavfile.write('stereo.wav', 44100, np.array([[1.0,  -1.0],
-                                             [0.75, -0.75],
-                                             [0.5,  -0.5],
-                                             [0.25, -0.25]], dtype='float32'))
+def uint32(number):
+    return struct.pack('<I', number)
 
-wavfile.write('mono.wav', 44100, np.array([0, 1, 2, -2, -1], dtype='int16'))
 
-platform.subprocess.call(['sox', 'mono.wav', 'mono.raw'])
+def uint16(number):
+    return struct.pack('<H', number)
+
+
+mono_raw = struct.pack('<5h', 0, 1, 2, -2, -1)
+
+stereo_raw = struct.pack('<8f',
+                         1.0,  -1.0,
+                         0.75, -0.75,
+                         0.5,  -0.5,
+                         0.25, -0.25)
+
+mono_data = (
+    b'RIFF' +
+    uint32(46) +  # size
+
+    b'WAVE' +  # file type
+
+    b'fmt ' +
+    uint32(16) +  # chunk size
+    uint16(1) +  # data type (PCM)
+    uint16(1) +  # channels
+    uint32(44100) +  # samplerate
+    uint32(44100 * 2) +  # bytes per second
+    uint16(2) +  # frame size
+    uint16(16) +  # bits per sample
+
+    b'data' +
+    uint32(5 * 2) +  # chunk size
+    mono_raw
+)
+
+stereo_data = (
+    b'RIFF' +
+    uint32(80) +  # size
+
+    b'WAVE' +  # file type
+
+    b'fmt ' +
+    uint32(16) +  # chunk size
+    uint16(3) +  # data type (float)
+    uint16(2) +  # channels
+    uint32(44100) +  # samplerate
+    uint32(44100 * 2 * 4) +  # bytes per second
+    uint16(2 * 4) +  # frame size
+    uint16(32) +  # bits per sample
+
+    b'fact' +
+    uint32(4) +  # chunk size
+    uint32(4) +  # number of frames
+
+    b'data' +
+    uint32(8 * 4) +  # chunk size
+    stereo_raw
+)
+
+with open('stereo.wav', 'wb') as f:
+    f.write(stereo_data)
+
+with open('mono.wav', 'wb') as f:
+    f.write(mono_data)
+
+with open('mono.raw', 'wb') as f:
+    f.write(mono_raw)
