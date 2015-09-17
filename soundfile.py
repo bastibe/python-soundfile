b078a3887bd91789b5f93d05fa88fee36dfe63dc
Matthias Geier <Matthias.Geier@gmail.com>
2015-09-17 19:38:06 +0200

    Change 2 comments to proper method docstrings
    ... and move one operator from after to before the line break.

--- soundfile.py
+++ soundfile.py
@@ -1086,11 +1086,11 @@ class SoundFile(object):
             raise ValueError("I/O operation on closed file")
 
     def _check_frames(self, frames, fill_value):
-        # Check if frames is larger than the remaining frames in the file
+        """Reduce frames to no more than are available in the file."""
         if self.seekable():
             remaining_frames = len(self) - self.tell()
-            if frames < 0 or (frames > remaining_frames
-                              and fill_value is None):
+            if frames < 0 or (frames > remaining_frames and
+                              fill_value is None):
                 frames = remaining_frames
         elif frames < 0:
             raise ValueError("frames must be specified for non-seekable files")
@@ -1135,7 +1135,7 @@ class SoundFile(object):
         return frames
 
     def _prepare_read(self, start, stop, frames):
-        # Seek to start frame and calculate length
+        """Seek to start frame and calculate length."""
         if start != 0 and not self.seekable():
             raise ValueError("start is only allowed for seekable files")
         if frames >= 0 and stop is not None:
