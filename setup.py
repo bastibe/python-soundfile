f832aca5d9b914cfd1031c75cff4fe4005576e9a
Matthias Geier <Matthias.Geier@gmail.com>
2017-02-02 09:19:35 +0100

    Extend supported wheel versions with cp36 and pp33

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ import sys
 
 PYTHON_INTERPRETERS = '.'.join([
     'cp26', 'cp27',
-    'cp32', 'cp33', 'cp34', 'cp35',
+    'cp32', 'cp33', 'cp34', 'cp35', 'cp36',
     'pp27',
-    'pp32',
+    'pp32', 'pp33',
 ])
 MACOSX_VERSIONS = '.'.join([
     'macosx_10_5_x86_64',
