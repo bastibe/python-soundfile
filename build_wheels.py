da2a650dfbb84c8c59a5bb0ec1d178ff70c67f5b
Bastian Bechtold <basti@bastibe.de>
2022-02-16 09:43:28 +0100

    fixes wheel build script
    - no longer leaks soundfile.egg-info directory, or environment
      variables.
    - builds a clean sdist without _soundfile_data.

--- build_wheels.py
+++ build_wheels.py
@@ -6,18 +6,21 @@ architectures = dict(darwin=['x86_64', 'arm64'],
                      noplatform='noarch')
 
 def cleanup():
+    os.environ['PYSOUNDFILE_PLATFORM'] = ''
+    os.environ['PYSOUNDFILE_ARCHITECTURE'] = ''
     shutil.rmtree('build', ignore_errors=True)
+    shutil.rmtree('soundfile.egg-info', ignore_errors=True)
     try:
         os.remove('_soundfile.py')
     except:
         pass
 
 for platform, archs in architectures.items():
-    os.environ['PYSOUNDFILE_PLATFORM'] = platform
     for arch in archs:
+        os.environ['PYSOUNDFILE_PLATFORM'] = platform
         os.environ['PYSOUNDFILE_ARCHITECTURE'] = arch
+        os.system('python3 setup.py bdist_wheel')
         cleanup()
-        os.system('python setup.py bdist_wheel')
 
+os.system('python3 setup.py sdist')
 cleanup()
-os.system('python setup.py sdist')
