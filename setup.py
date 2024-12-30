35d9ff6c31d840ed828124927b7151896918f620
Bastian Bechtold <bastibe.dev@mailbox.org>
2024-12-30 17:01:44 +0100

    removes setuptools.command.test dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 import os
 from platform import architecture, machine
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
 import sys
 
 # environment variables for cross-platform package creation
@@ -34,27 +33,7 @@ else:
     package_data = None
     zip_safe = True
 
-
-class PyTest(TestCommand):
-
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
-cmdclass = {'test': PyTest}
+cmdclass = {}
 
 try:
     from wheel.bdist_wheel import bdist_wheel
